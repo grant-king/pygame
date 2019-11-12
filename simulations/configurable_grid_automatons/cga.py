c17fb3a6b67c915b8bd5666dce3a36e9fa70d973
Grant King <grantryan.king@gmail.com>
2019-11-12 04:28:46 -0800

    Update cga.py

--- simulations/configurable_grid_automatons/cga.py
+++ simulations/configurable_grid_automatons/cga.py
@@ -6,9 +6,8 @@ import os
 import cv2
 
 SCREEN_SIZE = [1600, 900]
-CELL_SIZE = 1
+CELL_SIZE = 10
 BACKGROUND_COLOR = [0, 0, 0]
-DEAD_RATIO = 2 / 7
 
 def main_loop():
     pygame.init()
@@ -16,24 +15,16 @@ def main_loop():
     clock = pygame.time.Clock()
     main_window = pygame.display.set_mode(SCREEN_SIZE)
     
-    grid = Grid(CELL_SIZE, 'mazectric', aging=1)
+    grid = Grid(CELL_SIZE, 'conway', aging=1)
     capture = Capture(grid)
     control = Control(capture)
     
-    chances = list([1 for dead in range(int(1 / DEAD_RATIO - 1))])
-    chances.append(0)
-    seedline_cols = set([random.randrange(grid.num_columns) for column in range(random.randrange(10, grid.num_columns//5))])
-    for row_idx, cell_row in enumerate(grid.cells):
-        if row_idx in seedline_cols:
-            for cell in cell_row:
-                cell.toggle_cell(random.choice(chances))
-     
-    grid.manual_update_states()
-
+    #grid.random_seed()
     capture.load_image("D:/chaos/extra/st2_cropped.jpg")
+    control.step_clock.set_timer(QUIT, 1600) #auto off after n steps
+    control.step_clock.set_timer(control.STATESHOTEVENT, 10)
+
     while control.running:
-        clock.tick(19)
-        
         grid.update()
         capture.screen_shot()
         pygame.display.set_caption(f'{grid.rule_set.name} step {grid.rule_set.run_ticks}')
@@ -41,7 +32,6 @@ def main_loop():
         capture.save_image()
     #capture last state before quit        
     capture.state_shot()
-    capture.save_image()
     pygame.quit()
 
 if __name__ == '__main__':
