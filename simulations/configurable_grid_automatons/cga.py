6b30916e9b41a90012d5b197a844c68d35cad4af
Grant King <grantryan.king@gmail.com>
2019-11-15 23:55:20 -0800

    Update cga.py

--- simulations/configurable_grid_automatons/cga.py
+++ simulations/configurable_grid_automatons/cga.py
@@ -6,7 +6,7 @@ import os
 import cv2
 
 SCREEN_SIZE = [1600, 900]
-CELL_SIZE = 10
+CELL_SIZE = 2
 BACKGROUND_COLOR = [0, 0, 0]
 
 def main_loop():
@@ -15,15 +15,16 @@ def main_loop():
     clock = pygame.time.Clock()
     main_window = pygame.display.set_mode(SCREEN_SIZE)
     
-    grid = Grid(CELL_SIZE, 'conway', aging=1)
+    grid = Grid(CELL_SIZE, 'stains', aging=1)
     capture = Capture(grid)
     control = Control(capture)
     
     #grid.random_seed()
-    capture.load_image("D:/chaos/extra/st2_cropped.jpg")
-    control.step_clock.set_timer(QUIT, 1600) #auto off after n steps
-    control.step_clock.set_timer(control.STATESHOTEVENT, 10)
-
+    capture.load_image("D:/chaos/extra/cat_people.jpg")
+    control.step_clock.set_timer(QUIT, 300) #auto off after n steps
+    control.step_clock.set_timer(control.STATESHOTEVENT, 1) #capture state every n steps
+    
+    capture.state_shot()
     while control.running:
         grid.update()
         capture.screen_shot()
